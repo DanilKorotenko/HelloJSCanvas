a93bc0e93bb2f191f325d7f803c9fd0f6f82db52
Danil Korotenko <danil.korotenko@gmail.com>
2023-11-28 14:27:05 +0200

    Moved js to separate file.

---
+++ docs/index.js
@@ -0,0 +1,14 @@
+function draw() 
+{
+    const canvas = document.getElementById("canvas");
+    if (canvas.getContext) 
+    {
+        const ctx = canvas.getContext("2d");
+
+        ctx.fillRect(25, 25, 100, 100);
+        ctx.clearRect(45, 45, 60, 60);
+        ctx.strokeRect(50, 50, 50, 50);
+    }
+}
+
+window.addEventListener("load", draw);
